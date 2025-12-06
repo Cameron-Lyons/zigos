a584895278500dec19ae4c2fca78bf278910871f
Cameron Lyons <cameron.lyons2@gmail.com>
2025-12-05 23:23:59 -0600

    clean

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-nasm -f elf64 src/boot/boot.asm -o build/boot.o
-
-zig build kernel
-
-ld -n -T src/arch/x86_64/linker.ld -o zig-out/bin/kernel.elf build/boot.o zig-out/bin/kernel.elf.o
-
