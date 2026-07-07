3f2199bc1cac64d7ecb915563df6f792c8b11371
Cameron Lyons <cameron.lyons2@gmail.com>
2026-07-07 10:47:43 -0500

    Remove VGA text console

--- src/kernel/utils/console.zig
+++ src/kernel/utils/console.zig
@@ -1,14 +1,10 @@
-const vga = @import("../drivers/vga.zig");
 const serial = @import("../drivers/serial.zig");
 
 pub fn init() void {
-    vga.init();
-    vga.clear();
     serial.init();
 }
 
 pub fn print(str: []const u8) void {
-    vga.print(str);
     serial.print(str);
 }
 
@@ -17,19 +13,16 @@ pub fn printChar(c: u8) void {
 }
 
 pub fn putChar(c: u8) void {
-    vga.putChar(c);
     serial.putChar(c);
 }
 
 pub fn printWithColor(str: []const u8, color: u8) void {
-    vga.printWithColor(str, color);
+    _ = color;
     serial.print(str);
 }
 
-pub fn clear() void {
-    vga.clear();
-}
+pub fn clear() void {}
 
 pub fn clearWithColor(color: u8) void {
-    vga.clearWithColor(color);
+    _ = color;
 }
